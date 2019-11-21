02f865c25de3ad4cafd6394a4c4ef8e7f1463b7f
teach <teachliang@can-dao.com>
2019-11-21 15:44:58 +0800

    修复数据插入方法的bug;优化刷新列表方法和点击监听回调。

--- groupedadapter/src/main/java/com/donkingliang/groupedadapter/adapter/GroupedRecyclerViewAdapter.java
+++ groupedadapter/src/main/java/com/donkingliang/groupedadapter/adapter/GroupedRecyclerViewAdapter.java
@@ -1,7 +1,6 @@
 package com.donkingliang.groupedadapter.adapter;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -160,7 +159,6 @@ public abstract class GroupedRecyclerViewAdapter
     public int getItemCount() {
         if (isDataChanged) {
             structureChanged();
-            Log.e("eee", "---");
         }
         return count();
     }
